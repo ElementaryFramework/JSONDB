0962ae8b07a0715d0123f1057e8926972a56d9ff
Nana Axel <ax.lnana@outlook.com>
2016-08-28 02:39:37 +0200

    Update test suite

--- tests/JSONDBTest.php
+++ tests/JSONDBTest.php
@@ -14,7 +14,18 @@ class JSONDBTest extends PHPUnit_Framework_TestCase
     {
         // create db
         self::$database = new JSONDB();
-        self::$database->createServer('__phpunit_test_server', '__phpunit', '');
+        self::$database->createServer('__phpunit_test_server', '__phpunit', '', TRUE);
+        self::$database->createDatabase('__phpunit_test_database')->setDatabase('__phpunit_test_database');
+        self::$database->createTable('__phpunit_test_table', array('php' => array('type' => 'string'), 'unit' => array('type' => 'int')));
+        self::$database->disconnect();
+    }
+
+    /**
+     * @expectedException \JSONDB\Exception
+     */
+    public function testExceptionIsRaisedForExistingServer()
+    {
+        self::$database->createServer('__phpunit_test_server', '__phpunit', '', TRUE);
     }
 
     /**
@@ -30,7 +41,52 @@ class JSONDBTest extends PHPUnit_Framework_TestCase
      */
     public function testExceptionIsRaisedForInvalidUser()
     {
-        self::$database->connect('__phpunit_test_server', '__phpunit', '');
+        self::$database->connect('__phpunit_test_server', 'InvalidUser', '');
     }
 
+    /**
+     * @expectedException \JSONDB\Exception
+     */
+    public function testExceptionIsRaisedForInvalidPassword()
+    {
+        self::$database->connect('__phpunit_test_server', '__phpunit', 'InvalidPassword');
+    }
+
+    /**
+     * @expectedException \JSONDB\Exception
+     */
+    public function testExceptionIsRaisedForBadDatabaseConnection()
+    {
+        self::$database->disconnect();
+        self::$database->createDatabase('__phpunit_test_database');
+    }
+
+    /**
+     * @expectedException \JSONDB\Exception
+     */
+    public function testExceptionIsRaisedForExistingDatabase()
+    {
+        self::$database->disconnect();
+        self::$database->connect('__phpunit_test_server', '__phpunit', '');
+        self::$database->createDatabase('__phpunit_test_database');
+    }
+
+    /**
+     * @expectedException \JSONDB\Exception
+     */
+    public function testExceptionIsRaisedForBadDatabaseConnection2()
+    {
+        self::$database->disconnect();
+        self::$database->createTable('__phpunit_test_table', array('php' => array('type' => 'string'), 'unit' => array('type' => 'int')));
+    }
+
+    /**
+     * @expectedException \JSONDB\Exception
+     */
+    public function testExceptionIsRaisedForExistingTable()
+    {
+        self::$database->disconnect();
+        self::$database->connect('__phpunit_test_server', '__phpunit', '', '__phpunit_test_database');
+        self::$database->createTable('__phpunit_test_table', array('php' => array('type' => 'string'), 'unit' => array('type' => 'int')));
+    }
 }
