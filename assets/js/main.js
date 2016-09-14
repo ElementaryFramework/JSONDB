252cfda7a666729a8636f48349aef714ed38b102
Nana Axel <ax.lnana@outlook.com>
2016-09-14 06:54:41 +0200

    Update website
    Final website available at http://php.jsondb.na2axl.tk

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,23 +4,39 @@
 
         var $window = $(window);
 
-        GRID.init({
-            breakPoints: {
-                mobile: 480,
-                tablet: 1024
-            }
-        });
+        GRID.init();
 
         $.grid.menuToggle({
             wrapper: 'section#main',
             menu: 'nav.menu',
             breakPoint: 1024,
-            animation: 'slide-left'
+            animation: 'slide-left',
+            closeOnScroll: true
+        });
+        
+        $('div.class-show-list').on('click', function () {
+            $('section.class-desc').css('display', 'none');
+            $('aside.class-list').css('display', 'block');
+            $('div.class-hide-list').css('display', 'block');
+        });
+
+        $('div.class-hide-list').on('click', function () {
+            $('div.class-hide-list').css('display', 'none');
+            $('aside.class-list').css('display', 'none');
+            $('section.class-desc').css('display', 'block');
         });
 
         $.grid.scrollTo({
             scroller: ".scrollTo",
             speed: 2000
+        });
+
+        $('.tooltip').each(function () {
+            $(this).tooltip({
+                contentAsHTML: true,
+                position: $(this).attr('data-tooltip-position'),
+                content: $(this).attr('data-tooltip-content')
+            });
         });
 
         var AffixScrollSpy = $.grid.affix({
@@ -48,10 +64,10 @@
         });
 
         $window.on('resize', function() {
-            AffixClassList
+            AffixClassList && AffixClassList
                 .affix('setBottom', $('footer#footer').outerHeight(!0))
                 .affix('checkPosition');
-            AffixScrollSpy
+            AffixScrollSpy && AffixScrollSpy
                 .affix('setTop', $("article.manual-content").offset().top - $("header#header").height())
                 .affix('setBottom', $('html').outerHeight(!0) - $('#scrollspy-limit').offset().top - $("header#header").height())
                 .affix('checkPosition');
@@ -72,7 +88,45 @@
                     $a.addClass('current');
                 }
             });
-        }).trigger('scroll.docs.nav');
+        }).trigger('scroll');
+        
+        GRID.onBreakPointChange('mobile', function () {
+            $.grid.detach('.class-list', 'affix');
+            $('aside.class-list').css('display', 'none');
+            $('section.class-desc').css('display', 'block');
+        });
+
+        GRID.onBreakPointChange('tablet_narrow', function () {
+            $('aside.class-list').css('display', 'none');
+            $('section.class-desc').css('display', 'block');
+        });
+
+        GRID.onBreakPointChange('tablet_wide', function () {
+            $('aside.class-list').css('display', 'block');
+            $('section.class-desc').css('display', 'block');
+            AffixScrollSpy = $.grid.affix({
+                element: '.scrollspy',
+                offset: {
+                    top: function () {
+                        return this.top = $("article.manual-content").offset().top - $("header#header").height();
+                    },
+                    bottom: function () {
+                        return this.bottom = $('html').outerHeight(!0) - $('#scrollspy-limit').offset().top - $("header#header").height();
+                    }
+                }
+            });
+            AffixClassList = $.grid.affix({
+                element: '.class-list',
+                offset: {
+                    top: function(){
+                        return this.top = 1;
+                    },
+                    bottom: function(){
+                        return this.bottom = $('footer#footer').outerHeight(!0);
+                    }
+                }
+            });
+        });
     });
 
 })(jQuery);
